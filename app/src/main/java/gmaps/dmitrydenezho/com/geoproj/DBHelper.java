e73e57b292e531bc1699af8e2418978505b5fe8d
DimaDen94 <DimaDen94@MAIL.RU>
2016-01-16 02:47:06 +0200

    DB

---
+++ app/src/main/java/gmaps/dmitrydenezho/com/geoproj/DBHelper.java
@@ -0,0 +1,28 @@
+package gmaps.dmitrydenezho.com.geoproj;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Dmitry on 15.01.2016.
+ */
+public class DBHelper extends SQLiteOpenHelper {
+
+    public DBHelper(Context context) {
+        super(context, "myDB", null, 1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table photoDB("
+                + "id integer primary key autoincrement,"
+                + "photo LONGBLOB" + ");");
+
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
